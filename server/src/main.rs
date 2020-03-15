35bd6cc3476fe55ac3f0280d12a2cb417867090a
Celeo <celeodor@gmail.com>
2020-03-15 16:48:48 -0700

    Backend working

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,25 +1,43 @@
-use std::env;
+use std::{collections::HashMap, env};
 
 use lazy_static::lazy_static;
+use serde::Serialize;
+use stringsort::insertsort;
 use warp::{self, filters::BoxedFilter, Filter};
 
+// Include the English words in the binary.
 static _WORD_FILE_CONTENT: &str = include_str!("words_alpha.txt");
 
 lazy_static! {
-    static ref WORD_LIST: Vec<String> = _WORD_FILE_CONTENT
-        .split_whitespace()
-        .map(|s| s.to_owned())
-        .collect();
+    // Parse the English word list string into a map of sorted word to original word.
+    static ref WORD_LOOKUP: HashMap<String, String> = {
+        let word_list: Vec<&str> = _WORD_FILE_CONTENT.split_whitespace().collect();
+        let mut map = HashMap::new();
+        word_list.iter().for_each(|&word| {
+            map.insert(insertsort(word), word.to_owned());
+        });
+        map
+    };
 }
 
-#[tokio::main]
-async fn main() {
-    if env::var_os("RUST_LOG").is_none() {
-        env::set_var("RUST_LOG", "debug");
+/// JSON struct for the search handler.
+#[derive(Debug, Serialize)]
+struct SearchResponse {
+    original: String,
+    sorted: String,
+    matches: Option<String>,
+}
+
+impl SearchResponse {
+    /// Create a new response struct, processing the string.
+    fn new(word: &str) -> Self {
+        let sorted = insertsort(word);
+        SearchResponse {
+            original: word.to_owned(),
+            sorted: sorted.clone(),
+            matches: WORD_LOOKUP.get(&sorted).map(|s| s.to_owned()),
+        }
     }
-    pretty_env_logger::init();
-    let routes = route_search();
-    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
 
 /// Return the route for /search/:String
@@ -34,9 +52,16 @@ fn route_search() -> BoxedFilter<(impl warp::Reply,)> {
 /// Return the handler for taking letters and matching
 /// them against the list of English words.
 async fn handle_search(path_val: String) -> Result<impl warp::Reply, warp::Rejection> {
-    Ok(warp::reply::html(format!(
-        "Param: {}, is valid word: {}",
-        path_val,
-        WORD_LIST.contains(&path_val)
-    )))
+    Ok(warp::reply::json(&SearchResponse::new(&path_val)))
+}
+
+/// Entry point.
+#[tokio::main]
+async fn main() {
+    if env::var_os("RUST_LOG").is_none() {
+        env::set_var("RUST_LOG", "info");
+    }
+    pretty_env_logger::init();
+    let routes = route_search();
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
